c7b70819ca1b153ef7eeca6c3aa35f36bdbd2ebe
Steelerforreel <reelnicholas@gmail.com>
2023-11-06 21:49:05 -0500

    added server.js file for functionality

---
+++ server.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+const uniqid = require('uniqid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+app.post('/api/notes', (req, res) =>
+  
+)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
