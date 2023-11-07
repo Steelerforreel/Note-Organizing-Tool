1c02633d51d884f6a466c42c8b5ea6edec4a5e02
Steelerforreel <reelnicholas@gmail.com>
2023-11-06 22:06:31 -0500

    added get and post requests for server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./public/index.html');
 const uniqid = require('uniqid');
+const notes = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 
@@ -26,13 +27,25 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
-);
+app.get('/api/notes', (req, res) => {
+  return res.json(notes)
+});
 
-app.post('/api/notes', (req, res) =>
-  
-)
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a review`);
+
+  let response;
+
+  if (req.body) {
+    response = {
+        status: 'success',
+        data: req.body,
+};
+    res.json(`Note for ${response.data} has been added!`);
+} else {
+    res.json('Request body must contain something.');
+}
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
