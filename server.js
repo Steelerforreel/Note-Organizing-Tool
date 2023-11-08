ab4a3c90a0fd442794b4f153b9b0a1923c1e4b3f
Steelerforreel <reelnicholas@gmail.com>
2023-11-07 23:04:51 -0500

    Corrected files in server.js and added path to db.json file for posting

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const express = require('express');
 const path = require('path');
-const api = require('./public/index.html');
 const uniqid = require('uniqid');
 const notes = require('./db/db.json')
 
@@ -11,20 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -33,19 +23,22 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a review`);
-
-  let response;
-
-  if (req.body) {
-    response = {
-        status: 'success',
-        data: req.body,
-};
-    res.json(`Note for ${response.data} has been added!`);
-} else {
-    res.json('Request body must contain something.');
-}
+  const request = req.body
+  request.id = uniqid()
+  notes.push(request)
+  console.log(notes)
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+    if (err) {
+      throw err
+    }
+    res.json(request)
+  }
+  )
 });
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
